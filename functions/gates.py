563341fdacf1ee41967f17f51a61472384b12c6c
eliotpage <lgs19winse@lsf.org>
2025-01-18 12:42:04 +0000

    wokrs

--- functions/gates.py
+++ functions/gates.py
@@ -3,9 +3,9 @@
 
 def forget_gate(weights, input, hidden_state, debug=False):
     import numpy as np
-    Wf = weights[0][0]
-    Uf = weights[0][1]
-    Bf = weights[0][2]
+    Wf = weights[0]
+    Uf = weights[1]
+    Bf = weights[2]
     
     sum = hidden_state * Uf + input * Wf + Bf
 
@@ -26,9 +26,9 @@ def forget_gate(weights, input, hidden_state, debug=False):
 #BOTH USE THE INPUT AND HIDDEN STATE (SHORT TERM MEMORY) TO DO SO
 def input_gate_candidate(weights, input, hidden_state, debug=False):
     import numpy as np
-    Wf = weights[1][0]
-    Uf = weights[1][1]
-    Bf = weights[1][2]
+    Wf = weights[0][0]
+    Uf = weights[0][1]
+    Bf = weights[0][2]
     
     sum = hidden_state * Uf + input * Wf + Bf
 
@@ -45,9 +45,9 @@ def input_gate_candidate(weights, input, hidden_state, debug=False):
 
 def input_gate(weights, input, hidden_state, debug=False):
     import numpy as np
-    Wf = weights[2][0]
-    Uf = weights[2][1]
-    Bf = weights[2][2]
+    Wf = weights[1][0]
+    Uf = weights[1][1]
+    Bf = weights[1][2]
     
     sum = hidden_state * Uf + input * Wf + Bf
 
@@ -63,3 +63,37 @@ def input_gate(weights, input, hidden_state, debug=False):
     Ct = input_gate_candidate(weights, input, hidden_state)
 
     return It * Ct
+
+
+#OUTPUT GATE CONSISTS OF CANDIDATE CELL, WHICH CALCULATES POTENTIAL NEW SHORT TERM MEMORY, AND MAIN OUTPUT GATE,
+#WHICH CALCULATES THE % OF THE CANDIDATE OUTPUT TO USE AS NEW HIDDEN STATE (SHORT TERM MEMORY)
+def output_gate_candidate(cell_state):
+    import numpy as np
+    #OUTPUT CANDIDATE CELL SIMPLY INPUTS CELL STATE AFTER THE OTHER GATES INTO A TANH ACTIVATION FUNCTION
+    def tanh(x):
+        return (np.exp(x) - np.exp(-x)) / (np.exp(x) + np.exp(-x))
+    Ct = tanh(cell_state)
+
+    return Ct
+
+
+def output_gate(weights, input, hidden_state, cell_state, debug=False):
+    import numpy as np
+    Wf = weights[0]
+    Uf = weights[1]
+    Bf = weights[2]
+    
+    sum = hidden_state * Uf + input * Wf + Bf
+
+    if debug:
+        print('OUTPUT GATE:')
+        print(f'weights:{weights}\n\nWf:{Wf}\n\nUf:{Uf}\n\nBf:{Bf}\n\nhidden:{hidden_state}\n\ninput:{input}\n\nformula:{hidden_state} * {Uf} + {input} * {Wf} + {Bf}\n\nsum:{sum}')
+
+    #PLUG VALUE INTO SIGMOID ACTIVATION FUNCTION: f(x) = -1/1+e^-x
+    def sig(x):
+        return 1/(1 + np.exp(-x))
+    Ft = sig(sum)
+
+    Ct = output_gate_candidate(cell_state)
+
+    return Ft * Ct
--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-
 import numpy as np
 from functions.initializer import init as initialize_weights
 from functions.gates import *
+
 
 #CONSTANTS
 INPUT_SIZE = 3
@@ -12,25 +12,57 @@ hidden_state = 0
 cell_state = 0
 weights = initialize_weights(INPUT_SIZE, HIDDEN_SIZE)
 
+
+#RUN ONE FOWARD PASS THROUGH EVERY GATE
 def foward_pass(input, debug=False):
     global hidden_state, cell_state
+
+    #'WEIGHTS' CONTAINS (FORGET GATE WEIGHTS AND BIASES, INPUT CANDIDATE, INPUT GATE, OUTPUT GATE) IN THAT ORDER
+    #SO FORGET GATE USES FIRST, INPUT USES 2ND AND 3RD AND OUTPUT USES 4TH
 
     if debug:
         print(f'initial cell state: {cell_state}')
         print(f'initial hidden state: {hidden_state}')
-        print(f'\nFt: {forget_gate(weights, 1, hidden_state)}')
+        print(f'input: {input}')
 
-    cell_state *= forget_gate(weights, 1, hidden_state)
+
+    #UPDATE CELL STATE USING FORGET GATE
+    if debug:
+        print(f'\nFt: {forget_gate(weights[0], 1, hidden_state)}')
+
+    cell_state *= forget_gate(weights[0], 1, hidden_state)
+
     if debug:
         print(f'cell state after forget gate: {cell_state}')
         print(f'hidden state after forget gate: {hidden_state}')
-        print(f'\nIt: {input_gate(weights, 1, hidden_state)}')
 
-    cell_state += input_gate(weights, 1, hidden_state)
+
+    #UPDATE CELL STATE USING INPUT GATE
+    if debug:
+        print(f'\nIt: {input_gate((weights[1], weights[2]), 1, hidden_state)}')
+
+    cell_state += input_gate((weights[1], weights[2]), 1, hidden_state)
+
     if debug:
         print(f'cell state after input gate: {cell_state}')
         print(f'hidden state after input gate: {hidden_state}')
 
 
+    #UPDATE HIDDEN STATE USING OUTPUT GATE
+    if debug:
+        print(f'\nOt: {output_gate(weights[3], 1, hidden_state, cell_state)}')
+
+    hidden_state = output_gate(weights[3], 1, hidden_state, cell_state)
+
+    if debug:
+        print(f'cell state after output gate: {cell_state}')
+        print(f'hidden state after output gate: {hidden_state}')
+
+    return hidden_state, cell_state
+
+
 if __name__ == '__main__': 
-    foward_pass(1, debug=True)
+    foward_pass(1)
+    print(hidden_state, cell_state)
+    foward_pass(2)
+    print(hidden_state, cell_state)
